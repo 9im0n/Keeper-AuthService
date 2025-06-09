7dbb0ffabf51c38c8c7e39cfcfc48909b8f53fe6
9im0n <105603078+9im0n@users.noreply.github.com>
2025-06-09 22:08:51 +0300

    Revert "Dev"

--- Keeper-AuthService/Program.cs
+++ Keeper-AuthService/Program.cs
@@ -1,17 +1,8 @@
-using Keeper_AuthService.Services.Implementations;
+using Keeper_AuthService.Services.Implemitations;
 using Keeper_AuthService.Services.Interfaces;
-using Keeper_AuthService.Models.Services;
-using Keeper_AuthService.Models.Settings;
-using Keeper_AuthService.DB;
 using Microsoft.EntityFrameworkCore;
-using Keeper_AuthService.Repositories.Interfaces;
-using Keeper_AuthService.Repositories.Implementations;
-using Keeper_AuthService.Middlewares;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using DotNetEnv;
-
+using Keeper_AuthService.Models.Services;
+using Keeper_AuthService.Services.Implemintations;
 
 namespace Keeper_AuthService
 {
@@ -21,60 +12,13 @@ namespace Keeper_AuthService
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-            if (builder.Environment.IsDevelopment())
-                builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
-            else
-                Env.Load();
-
-            builder.Configuration.AddEnvironmentVariables();
-
             // Add services to the container
             builder.Services.AddHttpClient<IHttpClientService, HttpClientService>();
             builder.Services.Configure<ApiUrls>(builder.Configuration.GetSection("ApiUrls"));
 
-            // Configuration
-            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
-            builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailService"));
-
-            // Auth
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuer = builder.Configuration.GetSection("JwtSettings:issuer").Value,
-                        ValidateAudience = true,
-                        ValidAudience = builder.Configuration.GetSection("JwtSettings:audience").Value,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
-                            builder.Configuration.GetSection("JwtSettings:Key").Value!
-                            )),
-                        ValidAlgorithms = new string[] { SecurityAlgorithms.HmacSha256 },
-                    };
-                });
-
-            //Db
-            string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connection));
-
-            // Repositories
-            builder.Services.AddScoped<IRefreshTokensRepository, RefreshTokensRepository>();
-            builder.Services.AddScoped<IPendingActivationsRepository, PendingActivationsRepository>();
-
-
             // Services
-            builder.Services.AddScoped<IActivationPasswordService, ActivationPasswordService>();
+
             builder.Services.AddScoped<IAuthService, AuthService>();
-            builder.Services.AddScoped<IDTOMapper, DTOMapper>();
-            builder.Services.AddScoped<IEmailService, EmailService>();
-            builder.Services.AddScoped<IJwtService, JwtService>();
-            builder.Services.AddScoped<IPendingActivationService, PendingActivationService>();
-            builder.Services.AddScoped<IRefreshTokenService, RefreshTokenService>();
             builder.Services.AddScoped<IUserService, UserService>();
 
             builder.Services.AddControllers();
@@ -83,8 +27,6 @@ namespace Keeper_AuthService
             builder.Services.AddSwaggerGen();            
 
             var app = builder.Build();
-
-            app.UseMiddleware<ExceptionHandlingMiddleware>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
